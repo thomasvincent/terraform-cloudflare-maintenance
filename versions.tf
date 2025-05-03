6f7ddb19028d9af7199e509555dcfd2cb1640c80
Thomas Vincent <thomasvincent@gmail.com>
2025-05-03 16:12:37 -0700

    fix: add provider version constraints and security checks 🔒
    - Added version constraints for external and local providers
    
    - Fixed tflint warnings
    
    - Added tflint and tfsec to CI workflow

--- versions.tf
+++ versions.tf
@@ -5,5 +5,13 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 5.2"
     }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
   }
 }
